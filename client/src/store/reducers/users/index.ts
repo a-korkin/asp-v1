d5d2e8d9fb11f93326da6d7ddbb37dcb90534c15
a-korkin <a-korkin88@yandex.ru>
2021-09-29 15:40:25 +0300

    common state

--- client/src/store/reducers/users/index.ts
+++ client/src/store/reducers/users/index.ts
@@ -1,13 +1,19 @@
 import { IUserState, UserAction, UserActionsEnum } from "./types"
 
 const initialState: IUserState = {
+    isLoading: false,
+    error: "",
     users: []
 }
 
 export const usersReducer = (state: IUserState = initialState, action: UserAction): IUserState => {
     switch (action.type) {
+        case UserActionsEnum.SET_LOADING:
+            return { ...state, isLoading: action.payload };
+        case UserActionsEnum.SET_ERROR:
+            return { ...state, error: action.payload };
         case UserActionsEnum.GET_USERS:
-            return { users: action.payload };
+            return { ...state, users: action.payload };
         default:
             return state;
     }
--- client/src/store/reducers/users/types.ts
+++ client/src/store/reducers/users/types.ts
@@ -1,16 +1,29 @@
 import { IUser } from "../../../models/user";
+import { ICommonState } from "../common/types";
 
-export interface IUserState {
+export interface IUserState extends ICommonState {
     users: IUser[]
 }
 
 export enum UserActionsEnum {
-    GET_USERS = "GET_USERS"
+    SET_LOADING = "SET_LOADING",
+    SET_ERROR = "SET_ERROR",
+    GET_USERS = "GET_USERS",
 }
 
 export interface GetUserAction {
-    type: UserActionsEnum.GET_USERS,
-    payload: IUser[]
+    type: UserActionsEnum.GET_USERS;
+    payload: IUser[];
 }
 
-export type UserAction = GetUserAction;
+export interface SetLoadingAction {
+    type: UserActionsEnum.SET_LOADING;
+    payload: boolean;
+}
+
+export interface SetErrorAction {
+    type: UserActionsEnum.SET_ERROR;
+    payload: string;
+}
+
+export type UserAction = GetUserAction | SetLoadingAction | SetErrorAction;
